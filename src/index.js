4dda5c99f5af16371fce0bc6761cab38a0cbfdb7
Sara McDonnell <saramcdonnell32@gmail.com>
2023-02-15 17:48:42 -0500

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(newTaskDescription.value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.textContent = "x";
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
